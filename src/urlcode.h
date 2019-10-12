ad3ca087c85a74ba54538d5a91abd7962759cb65
Lasse Karstensen <lasse.karstensen@gmail.com>
2019-10-12 14:12:20 +0200

    Nudging forwards..

---
+++ src/urlcode.h
@@ -0,0 +1,5 @@
+/*
+ *
+ */
+uint32_t urlencode(const char *, char *, int);
+uint32_t urldecode(const char *, char *, int);
--- src/vmod_encoding.c
+++ src/vmod_encoding.c
@@ -6,6 +6,7 @@
 #include "cache/cache.h"
 
 #include "vcc_encoding_if.h"
+#include "urlcode.h"
 
 int v_matchproto_(vmod_event_f)
 vmod_event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
@@ -17,15 +18,43 @@ vmod_event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
 VCL_STRING
 vmod_urlencode(VRT_CTX, VCL_STRING s)
 {
-	(void)ctx;
-	return(NULL);
+	if (s == NULL) {
+		return(NULL);
+	}
+	int destlen = WS_ReserveAll(ctx->ws);
+	AN(destlen);
+
+	int n = urlencode(s, ctx->ws->f, destlen-1);
+
+	if (n < 0) {
+		WS_Release(ctx->ws, 0);
+		return(NULL);
+	}
+
+	char *value = ctx->ws->f;
+	WS_Release(ctx->ws, n);
+	return(value);
 }
 
 VCL_STRING
 vmod_urldecode(VRT_CTX, VCL_STRING s)
 {
-	(void)ctx;
-	return(NULL);
+	if (s == NULL) {
+		return(NULL);
+	}
+	int destlen = WS_ReserveAll(ctx->ws);
+	AN(destlen);
+
+	int n = urldecode(s, ctx->ws->f, destlen-1);
+
+	if (n < 0) {
+		WS_Release(ctx->ws, 0);
+		return(NULL);
+	}
+
+	char *value = ctx->ws->f;
+	WS_Release(ctx->ws, n);
+	return(value);
 }
 
 VCL_STRING
